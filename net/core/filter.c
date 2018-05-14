6ca5ad70af084b0ecde8b2b712b5aa568716057c
John Fastabend <john.fastabend@gmail.com>
2018-05-14 10:00:16 -0700

    bpf: sockmap, refactor sockmap routines to work with hashmap
    This patch only refactors the existing sockmap code. This will allow
    much of the psock initialization code path and bpf helper codes to
    work for both sockmap bpf map types that are backed by an array, the
    currently supported type, and the new hash backed bpf map type
    sockhash.
    
    Most the fallout comes from three changes,
    
      - Pushing bpf programs into an independent structure so we
        can use it from the htab struct in the next patch.
      - Generalizing helpers to use void *key instead of the hardcoded
        u32.
      - Instead of passing map/key through the metadata we now do
        the lookup inline. This avoids storing the key in the metadata
        which will be useful when keys can be longer than 4 bytes. We
        rename the sk pointers to sk_redir at this point as well to
        avoid any confusion between the current sk pointer and the
        redirect pointer sk_redir.
    
    Change-Id: If606678100e2c9dc59ed0290a0570abe1a50d48f
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1932,9 +1932,10 @@ BPF_CALL_4(bpf_sk_redirect_map, struct sk_buff *, skb,
 	if (unlikely(flags))
 		return SK_DROP;
 
-	tcb->bpf.key = key;
 	tcb->bpf.flags = flags;
-	tcb->bpf.map = map;
+	tcb->bpf.sk_redir = __sock_map_lookup_elem(map, key);
+	if (!tcb->bpf.sk_redir)
+		return SK_DROP;
 
 	return SK_PASS;
 }
@@ -1942,16 +1943,7 @@ BPF_CALL_4(bpf_sk_redirect_map, struct sk_buff *, skb,
 struct sock *do_sk_redirect_map(struct sk_buff *skb)
 {
 	struct tcp_skb_cb *tcb = TCP_SKB_CB(skb);
-	struct sock *sk = NULL;
-
-	if (tcb->bpf.map) {
-		sk = __sock_map_lookup_elem(tcb->bpf.map, tcb->bpf.key);
-
-		tcb->bpf.key = 0;
-		tcb->bpf.map = NULL;
-	}
-
-	return sk;
+	return tcb->bpf.sk_redir;
 }
 
 static const struct bpf_func_proto bpf_sk_redirect_map_proto = {
@@ -1971,25 +1963,17 @@ BPF_CALL_4(bpf_msg_redirect_map, struct sk_msg_buff *, msg,
 	if (unlikely(flags))
 		return SK_DROP;
 
-	msg->key = key;
 	msg->flags = flags;
-	msg->map = map;
+	msg->sk_redir = __sock_map_lookup_elem(map, key);
+	if (!msg->sk_redir)
+		return SK_DROP;
 
 	return SK_PASS;
 }
 
 struct sock *do_msg_redirect_map(struct sk_msg_buff *msg)
 {
-	struct sock *sk = NULL;
-
-	if (msg->map) {
-		sk = __sock_map_lookup_elem(msg->map, msg->key);
-
-		msg->key = 0;
-		msg->map = NULL;
-	}
-
-	return sk;
+	return msg->sk_redir;
 }
 
 static const struct bpf_func_proto bpf_msg_redirect_map_proto = {
