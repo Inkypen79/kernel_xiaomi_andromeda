380e91b3d30c48ced9b7b2f673e3ba0f9233e8e0
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-22 13:48:53 +0000

    net: Guard BPF_GET_COMM_HASH uapi for MIUI builds
    Commit: 55fc8bd1edbc0de0ec7c18118ae2c0cb5e92f0b7
    BPF_GET_COMM_HASH seems to be breaking android13 bpf loader.
    Therefore, disable it for non MIUI builds to allow bpf loader
    to boot on android13 ROMs.
    
    Change-Id: I9f5c74760a75221d5ee8c8ead6c6f84a2dd5f812
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Bartłomiej Rudecki <ejbtrd@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1569,6 +1569,7 @@ static int bpf_obj_get_info_by_fd(const union bpf_attr *attr,
 	return err;
 }
 
+#if IS_ENABLED(CONFIG_MIHW)
 static int bpf_get_comm_hash(union bpf_attr *attr)
 {
 	void __user *uhash = u64_to_user_ptr(attr->hash);
@@ -1593,7 +1594,7 @@ static int bpf_get_comm_hash(union bpf_attr *attr)
 		return -EFAULT;
 	return 0;
 }
-
+#endif
 
 SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, size)
 {
@@ -1670,9 +1671,11 @@ SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, siz
 	case BPF_OBJ_GET_INFO_BY_FD:
 		err = bpf_obj_get_info_by_fd(&attr, uattr);
 		break;
+#if IS_ENABLED(CONFIG_MIHW)
 	case BPF_GET_COMM_HASH:
 		err = bpf_get_comm_hash(&attr);
 		break;
+#endif
 	default:
 		err = -EINVAL;
 		break;
