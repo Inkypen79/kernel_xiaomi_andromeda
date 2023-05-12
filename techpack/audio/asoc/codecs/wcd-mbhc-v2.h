1468e79b93f85b6b350aa44b636de794dbd39207
Nicholas Magill <nmagill1979@gmail.com>
2023-05-13 00:22:34 +0100

    techpack: audio: Restore fsa4476 audio switch functionality

--- techpack/audio/asoc/codecs/wcd-mbhc-v2.h
+++ techpack/audio/asoc/codecs/wcd-mbhc-v2.h
@@ -418,11 +418,17 @@ enum mbhc_hs_pullup_iref_v2 {
 
 struct usbc_ana_audio_config {
 	int usbc_en1_gpio;
-	int usbc_en2n_gpio;
+	int usbc_en2_gpio;
 	int usbc_force_gpio;
+	int euro_us_hw_switch_gpio;
+	int uart_audio_switch_gpio;
+	int subpcb_id_gpio;
 	struct device_node *usbc_en1_gpio_p; /* used by pinctrl API */
-	struct device_node *usbc_en2n_gpio_p; /* used by pinctrl API */
+	struct device_node *usbc_en2_gpio_p; /* used by pinctrl API */
 	struct device_node *usbc_force_gpio_p; /* used by pinctrl API */
+	struct device_node *euro_us_hw_switch_gpio_p; /* used by pinctrl API */
+	struct device_node *uart_audio_switch_gpio_p; /* used by pinctrl API */
+	struct device_node *subpcb_id_gpio_p; /* used by pinctrl API */
 };
 
 enum mbhc_moisture_rref {
@@ -449,7 +455,7 @@ struct wcd_mbhc_config {
 	u32 enable_usbc_analog;
 	bool moisture_duty_cycle_en;
 	struct usbc_ana_audio_config usbc_analog_cfg;
-	bool fsa_enable;
+	u32 use_fsa4476_gpio;
 	void (*enable_dual_adc_gpio)(struct device_node *node, bool en);
 	struct device_node *dual_adc_gpio_node;
 };
