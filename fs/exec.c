0219b11d320fbdcc5b04c4bb46c776b7a6de72f7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-22 22:53:48 +0800

    KernelSU: integrate scope-minimized manual hooks - k4.14 v1.5
    This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks.
    This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat).
    It reduces the scope of kernel function interception and still maintains full fucntionality.
    
    This commit is a squash of the following:
    *	fs/exec: do_execve: ksu_handle_execve_sucompat hook
    *	fs/exec: compat_do_execve: ksu_handle_execve_sucompat hook
    	fs/open: sys_faccessat: ksu_handle_faccessat hook
    *	fs/read_write: sys_read: ksu_handle_sys_read hook
    *	fs/stat: sys_newfstatat: ksu_handle_stat hook
    *	fs/stat: sys_fstatat64: ksu_handle_stat hook
    *	drivers: input: input_event: ksu_handle_input_handle_event hook
    
    references: KernelSU pr#1657, pr#2084
    	https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1977,11 +1977,22 @@ void set_dumpable(struct mm_struct *mm, int value)
 	} while (cmpxchg(&mm->flags, old, new) != old);
 }
 
+#ifdef CONFIG_KSU
+extern __attribute__((hot)) int ksu_handle_execve_sucompat(int *fd,
+				const char __user **filename_user,
+				void *__never_use_argv,
+				void *__never_use_envp,
+				int *__never_use_flags);
+#endif
+
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return do_execve(getname(filename), argv, envp);
 }
 
@@ -2003,6 +2014,9 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
+#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
+	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return compat_do_execve(getname(filename), argv, envp);
 }
 
