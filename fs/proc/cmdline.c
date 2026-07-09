3a0a8e9cd59b4cd02b3531b615839a3b78098ce7
Nicholas Magill <nmagill1979@gmail.com>
2026-07-09 21:26:39 +0200

    fs: Import susfs v2.2.0
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/ed306cc62cb0fa655982000a95b6f4832d21eb56

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -12,6 +12,11 @@
 #define INITRAMFS_STR_REPLACE "want_initramfs"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern struct static_key_false susfs_is_fake_cmdline_or_bootconfig_buffer_set;
+extern void susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
@@ -29,6 +34,13 @@ static void proc_command_line_init(void) {
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (static_branch_likely(&susfs_is_fake_cmdline_or_bootconfig_buffer_set)) {
+		susfs_spoof_cmdline_or_bootconfig(m);
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_printf(m, "%s\n", proc_command_line);
 #else
